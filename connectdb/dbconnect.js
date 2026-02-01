9a3cd313c9294504e1e84d52bd46a6641f2a378e
Hariom Sagitra <hariomdhakad750910@gmail.com>
2026-02-01 13:14:05 +0530

    connectdb upload

---
+++ connectdb/dbconnect.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+// mongoose.connect("mongodb://127.0.0.1:27017")
+
+const connectDB = async(DB_URL,DB_NAME)=>{
+   try {
+    //await mongoose.connect(DB_URL+DB_NAME)
+    await mongoose.connect("mongodb+srv://hariomsagitra750910_db_user:XmEtsVoBKAO3vlSl@cluster0.hlbfz8a.mongodb.net/?appName=Cluster0")
+    console.log("Database Connected Successfully!!")
+   } catch (error) {
+    console.log("Database not connected",error)
+   } 
+}
+
+export default connectDB;
