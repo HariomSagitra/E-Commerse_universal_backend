6f062c2bbebc71b17be1ab68b2b1d0ea9a559709
Hariom Sagitra <hariomdhakad750910@gmail.com>
2026-02-01 16:33:45 +0530

    Refactor database connection to use parameters
    Updated the database connection to use dynamic URL and name.

--- connectdb/dbconnect.js
+++ connectdb/dbconnect.js
@@ -1,10 +1,26 @@
+// import mongoose from "mongoose";
+// // mongoose.connect("mongodb://127.0.0.1:27017")
+
+// const connectDB = async(DB_URL,DB_NAME)=>{
+//    try {
+//     //await mongoose.connect(DB_URL+DB_NAME)
+//     await mongoose.connect("mongodb+srv://hariomsagitra750910_db_user:XmEtsVoBKAO3vlSl@cluster0.hlbfz8a.mongodb.net/?appName=Cluster0")
+//     console.log("Database Connected Successfully!!")
+//    } catch (error) {
+//     console.log("Database not connected",error)
+//    } 
+// }
+
+// export default connectDB;
+
 import mongoose from "mongoose";
 // mongoose.connect("mongodb://127.0.0.1:27017")
 
 const connectDB = async(DB_URL,DB_NAME)=>{
    try {
     //await mongoose.connect(DB_URL+DB_NAME)
-    await mongoose.connect("mongodb+srv://hariomsagitra750910_db_user:XmEtsVoBKAO3vlSl@cluster0.hlbfz8a.mongodb.net/?appName=Cluster0")
+    //await mongoose.connect("mongodb+srv://hariomsagitra750910_db_user:XmEtsVoBKAO3vlSl@cluster0.hlbfz8a.mongodb.net/?appName=Cluster0")
+    await mongoose.connect(`${DB_URL}${DB_NAME}`)
     console.log("Database Connected Successfully!!")
    } catch (error) {
     console.log("Database not connected",error)
