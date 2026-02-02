630eba4f788398f92088ef1f9e9a8cf4bb274b2d
Hariom Sagitra <hariomdhakad750910@gmail.com>
2026-02-03 02:27:12 +0530

    Switch to Cloudinary storage for file uploads

--- modal/singleUpload.js
+++ modal/singleUpload.js
@@ -1,15 +1,31 @@
+// import multer from "multer";
+// import {getDateTime} from '../utility/GetDate.js'
+
+
+// var storage_engine = multer.diskStorage({
+//     destination: (req, file, callback) => {
+//         callback(null, './uploaddocuments')
+//     },
+//     filename: (req, file, callback) => {
+//         callback(null, getDateTime() + "_" + file.originalname)
+//     }
+// })
+// var imgupload = multer({storage:storage_engine})
+// export default imgupload
+
 import multer from "multer";
-import {getDateTime} from '../utility/GetDate.js'
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import cloudinary from "../config/cloudinary.js";
 
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "ecommerce-products",
+    allowed_formats: ["jpg", "png", "jpeg", "webp"],
+  },
+});
 
-var storage_engine = multer.diskStorage({
-    destination: (req, file, callback) => {
-        callback(null, './uploaddocuments')
-    },
-    filename: (req, file, callback) => {
-        callback(null, getDateTime() + "_" + file.originalname)
-    }
-})
-var imgupload = multer({storage:storage_engine})
-export default imgupload
+const upload = multer({ storage });
+
+export default upload;
 
