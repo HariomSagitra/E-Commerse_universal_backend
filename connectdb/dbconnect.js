60586a5863b40b00ceccda277e1009930eb25bdb
Hariom Sagitra <hariomdhakad750910@gmail.com>
2026-02-03 01:26:30 +0530

    Refactor database connection in dbconnect.js
    Updated the database connection logic to use a dynamic URL and name.

--- connectdb/dbconnect.js
+++ connectdb/dbconnect.js
@@ -1,25 +1,10 @@
-// import mongoose from "mongoose";
-// // mongoose.connect("mongodb://127.0.0.1:27017")
 
-// const connectDB = async(DB_URL,DB_NAME)=>{
-//    try {
-//     //await mongoose.connect(DB_URL+DB_NAME)
-//     await mongoose.connect("mongodb+srv://hariomsagitra750910_db_user:XmEtsVoBKAO3vlSl@cluster0.hlbfz8a.mongodb.net/?appName=Cluster0")
-//     console.log("Database Connected Successfully!!")
-//    } catch (error) {
-//     console.log("Database not connected",error)
-//    } 
-// }
-
-// export default connectDB;
 
 import mongoose from "mongoose";
-// mongoose.connect("mongodb://127.0.0.1:27017")
+
 
 const connectDB = async(DB_URL,DB_NAME)=>{
    try {
-    //await mongoose.connect(DB_URL+DB_NAME)
-    //await mongoose.connect("mongodb+srv://hariomsagitra750910_db_user:XmEtsVoBKAO3vlSl@cluster0.hlbfz8a.mongodb.net/?appName=Cluster0")
     await mongoose.connect(`${DB_URL}${DB_NAME}`)
     console.log("Database Connected Successfully!!")
    } catch (error) {
