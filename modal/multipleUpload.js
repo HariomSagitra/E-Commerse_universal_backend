4e3f42ed185644bb4a737d0079d4b543863c7bb4
Hariom Sagitra <hariomdhakad750910@gmail.com>
2026-02-03 02:27:38 +0530

    Switch to Cloudinary for image uploads

--- modal/multipleUpload.js
+++ modal/multipleUpload.js
@@ -1,14 +1,29 @@
+// import multer from "multer";
+// import {getDateTime} from '../utility/GetDate.js'
+
+// var storage_engine = multer.diskStorage({
+//     destination: (req, file, callback) => {
+//         callback(null, './multipleuploadproducts')
+//     },
+//     filename: (req, file, callback) => {
+//         callback(null, getDateTime() + "_" + file.originalname)
+//     }
+// })
+// var imgupload = multer({storage:storage_engine})
+// export default imgupload
+
 import multer from "multer";
-import {getDateTime} from '../utility/GetDate.js'
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import cloudinary from "../config/cloudinary.js";
 
-var storage_engine = multer.diskStorage({
-    destination: (req, file, callback) => {
-        callback(null, './multipleuploadproducts')
-    },
-    filename: (req, file, callback) => {
-        callback(null, getDateTime() + "_" + file.originalname)
-    }
-})
-var imgupload = multer({storage:storage_engine})
-export default imgupload
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "ecommerce-products",
+    allowed_formats: ["jpg", "png", "jpeg", "webp"],
+  },
+});
 
+const upload = multer({ storage });
+
+export default upload;
